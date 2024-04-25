b9e7e64166016edca52b0598358e92430688fcf7
iplnkr <iphoplunker@gmail.com>
2024-04-25 22:48:09 +0100

    Updated Menu Screen

--- PowerGame/Assets/Scripts/Menu/LoadLevel.cs
+++ PowerGame/Assets/Scripts/Menu/LoadLevel.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadLevel : MonoBehaviour
 {
+    [SerializeField] private Animator building;
+    [SerializeField] private GameObject logo;
+    [SerializeField] private GameObject button;
+    [SerializeField] private GameObject pane;
     public void LoadScene(string levelName)
     {
+        if(pane != null)
+        {
+            pane.SetActive(true);
+        }
         SceneManager.LoadScene(levelName);
+    }
+
+    public void BuildingBlackout()
+    {
+        building.Play("Base Layer.HouseBlackout");
+        logo.SetActive(false);
+        button.SetActive(false);
     }
 }
